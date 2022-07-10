66f384c2d6a4b917a5e1f2a1a2089b0157aeb9ec
Ura <Yurick1771081@gmail.com>
2022-07-11 00:04:44 +0300

    create package config add in it class 'MySpringMvcDispatcherServletInitializer' witch the extends 'AbstractDispatcherServletInitializer'

---
+++ src/main/java/by/home/chaplinskiy/springmvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package by.home.chaplinskiy.springmvc.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractDispatcherServletInitializer {
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+}
