17af07660ee72b60576aa44ccc0eca3db8c9747d
Ura <Yurick1771081@gmail.com>
2022-07-10 23:40:58 +0300

    creat project with a web app archetype, add dependency spring 'core,context,mvc,web' and  thymeleaf. Add file web.xml and the applicationContextMVC.xml. Creat the class 'HelloController'

---
+++ src/main/java/by/home/chaplinskiy/springmvc/HelloController.java
@@ -0,0 +1,12 @@
+package by.home.chaplinskiy.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/yura")
+    public String sayHello() {
+        return "hello Yura";
+    }
+}
